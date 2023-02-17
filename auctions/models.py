d36c44146f498bb11679e7d17573d338bcacf383
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2023-02-18 01:49:03 +0800

    Update models

--- auctions/models.py
+++ auctions/models.py
@@ -22,9 +22,9 @@ class Product(models.Model):
     bids = models.PositiveIntegerField()
     seller = models.CharField(max_length=150)
     image_path = models.ImageField(upload_to=user_directory_path)
-    description = models.TextField()
+    description = models.TextField(blank=True)
 
-    category = models.ForeignKey(Categories, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Categories, on_delete=models.SET_NULL, null=True)
 
     def __str__(self) -> str:
         return f"Product name: {self.prod_name} \n\
@@ -41,9 +41,9 @@ class User(AbstractUser):
     auc_list and watchlist which are used to connect the many-to-many 
     relationship with Product.
     """
-    auc_list = models.ManyToManyField(Product, through='AuctionList')
-    watchlist = models.ManyToManyField(Product, through='Watchlist')
-    comments = models.ManyToManyField(Product, through='Comments')
+    auc_list = models.ManyToManyField(Product, through='AuctionList', related_name="prod_auctionlist")
+    watchlist = models.ManyToManyField(Product, through='Watchlist', related_name="prod_watchlist")
+    comments = models.ManyToManyField(Product, through='Comments', related_name="prod_cmt")
 
     def __str__(self) -> str:
         return f"Username: {self.username} \n\
@@ -56,8 +56,8 @@ class AuctionList(models.Model):
     An extra field on the many-to-many relationship between User and Product,
     representing the auction listing.
     """
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_auc_set")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="prod_auc_set")
 
     def __str__(self) -> str:
         return f"{self.user}: {self.product}"
@@ -65,11 +65,11 @@ class AuctionList(models.Model):
 
 class Watchlist(models.Model):
     """
-    An extra field on the many-to-many relationship between User and Product,
+    An extra field on the many-to-many relationship beappnametween User and Product,
     representing the watchlist.
     """
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_watch_set")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="prod_watch_set")
 
     def __str__(self) -> str:
         return f"{self.user}: {self.product}"
@@ -80,8 +80,8 @@ class Comments(models.Model):
     An extra field on the many-to-many relationship between User and Product,
     representing the comments.
     """
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_cmt_set")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="prod_cmt_set")
     comment = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
