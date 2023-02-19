d1e081b31a1e54ddd38091e2f7755ed31b943424
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2023-02-19 21:20:44 +0800

    Update MEDIA_ROOT

--- commerce/settings.py
+++ commerce/settings.py
@@ -138,4 +138,4 @@ DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
 # File path for media
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'auctions/static/media')
