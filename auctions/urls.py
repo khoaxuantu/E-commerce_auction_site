35357b4fe5c13dd34d70625a052f9ef962096bb7
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2023-02-28 23:11:55 +0800

    reorganize paths with 'close bid' and 'archive'

--- auctions/urls.py
+++ auctions/urls.py
@@ -4,15 +4,17 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
-    path("createlisting", views.create_listing, name="create listing"),
-    path("listings/<int:product_id>", views.listing_page, name="listings"),
+    path("accounts/login/", views.login_view, name="login"),
+    path("accounts/logout/", views.logout_view, name="logout"),
+    path("accounts/register/", views.register, name="register"),
     path("category", views.categories_view, name="category view"),
     path("category/<int:category_id>", views.categories, name="category"),
+    path("comment/<int:product_id>", views.add_comment, name="comment"),
+    path("listings/create", views.create_listing, name="create listing"),
+    path("listings/<int:product_id>", views.listing_page, name="listings"),
+    path("listings/close/<int:product_id>/<int:winner_id>", views.close_bid, name="close bid"),
+    path("listings/archive/<int:product_id>", views.archive_view, name="archive product"),
     path("watchlist", views.watchlist_page, name="watchlist"),
-    path("addtowatchlist/<int:product_id>", views.addto_watchlist, name="add to watchlist"),
-    path("unwatch/<int:product_id>/<int:user_id>", views.delete_from_watchlist, name="unwatch"),
-    path("comment/<int:product_id>", views.add_comment, name="comment")
+    path("watchlist/add/<int:product_id>", views.addto_watchlist, name="add to watchlist"),
+    path("watchlist/unwatch/<int:product_id>/<int:user_id>", views.delete_from_watchlist, name="unwatch"),
 ]
