bfd95da76d3e9fe37280a13993c1156c928ef1f3
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2023-03-18 21:01:35 +0800

    Update path terms

--- auctions/urls.py
+++ auctions/urls.py
@@ -10,11 +10,11 @@ urlpatterns = [
     path("category", views.categories_view, name="category view"),
     path("category/<int:category_id>", views.categories, name="category"),
     path("comment/<int:product_id>", views.add_comment, name="comment"),
-    path("listings/create", views.create_listing, name="create listing"),
+    path("listings/new", views.create_listing, name="create listing"),
     path("listings/<int:product_id>", views.listing_page, name="listings"),
     path("listings/close/<int:product_id>/<int:winner_id>", views.close_bid, name="close bid"),
     path("listings/archive/<int:product_id>", views.archive_view, name="archive product"),
     path("watchlist", views.watchlist_page, name="watchlist"),
-    path("watchlist/add/<int:product_id>", views.addto_watchlist, name="add to watchlist"),
+    path("watchlist/new/<int:product_id>", views.addto_watchlist, name="add to watchlist"),
     path("watchlist/unwatch/<int:product_id>/<int:user_id>", views.delete_from_watchlist, name="unwatch"),
 ]
